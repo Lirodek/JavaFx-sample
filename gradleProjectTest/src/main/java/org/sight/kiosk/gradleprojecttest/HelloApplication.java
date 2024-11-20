4bde7dfa888cac756657051bdb0cd5e1a799c703
lirodek0106 <lirodek0106@gmail.com>
2024-11-20 09:01:37 +0900

    commited
    second commit

--- gradleProjectTest/src/main/java/org/sight/kiosk/gradleprojecttest/HelloApplication.java
+++ gradleProjectTest/src/main/java/org/sight/kiosk/gradleprojecttest/HelloApplication.java
@@ -1,23 +1,43 @@
 package org.sight.kiosk.gradleprojecttest;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static Stage primaryStageObj;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+
+        primaryStageObj = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/main-view.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Kiosk-system Demo");
+        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResource("images/icon.png").toString()));
+
+        Scene scene = new Scene(root, 600, 400);
+        scene.setRoot(root);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static Stage getPrimaryStage() {
+        return primaryStageObj;
     }
 }
--- gradleProjectTest/src/main/java/org/sight/kiosk/gradleprojecttest/HelloController.java
+++ gradleProjectTest/src/main/java/org/sight/kiosk/gradleprojecttest/HelloController.java
@@ -1,5 +1,6 @@
 package org.sight.kiosk.gradleprojecttest;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
@@ -7,6 +8,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    int 증명서_발급_횟수 = 0;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -16,17 +18,27 @@ public class HelloController {
             Alert success = new Alert(Alert.AlertType.INFORMATION);
             success.setTitle("결과");
             success.setHeaderText("성공!");
-            success.setContentText("증명서 발급의 성공하였습니다!");
+            success.setContentText("증명서 발급의 성공하였습니다!\n 증명서 발급횟수 :: " + ++증명서_발급_횟수);
             success.showAndWait();
         } else {
             // 실패 (50% 확률)
             Alert fail = new Alert(Alert.AlertType.ERROR);
             fail.setTitle("결과");
             fail.setHeaderText("실패!");
-            fail.setContentText("50% 확률로 증명서 발급의 실패하였습니다!");
+            fail.setContentText("50% 확률로 증명서 발급의 실패하였습니다!\n 증명서 발급횟수 :: " + 증명서_발급_횟수);
             fail.showAndWait();
         }
     }
+
+    /* Terminates Application */
+    public void closeSystem(){
+        Platform.exit();
+        System.exit(0);
+    }
+
+//    public void minimizeWindow(){
+//        MainLauncher.getPrimaryStage().setIconified(true);
+//    }
 
     @FXML
     protected void onHelloButtonClick2() {welcomeText.setText("안녕하세요 JavaFx에 오신것을 환영합니다.");}
